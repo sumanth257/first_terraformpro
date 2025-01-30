0efcbf14028ca7777ea6edeea15e28e7ab6c56ac
sumanth257 <126243012+sumanth257@users.noreply.github.com>
2025-01-30 11:58:25 +0530

    Update main.tf

--- write_your_first_terraform_project-main/write_your_first_terraform_project-main/aws/local_state/main.tf
+++ write_your_first_terraform_project-main/write_your_first_terraform_project-main/aws/local_state/main.tf
@@ -10,11 +10,11 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "us-east-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-04b4f1a9cf54c11d0"
   instance_type = "t2.micro"
 
   tags = {
